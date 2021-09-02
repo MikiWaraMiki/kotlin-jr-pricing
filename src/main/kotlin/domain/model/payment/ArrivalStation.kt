195727ddffcb96233089293eeaf864e8d72cc391
MikiWaraMiki <a.i.u.e.o.1.2.3.a@gmail.com>
2021-09-02 22:00:35 +0900

    refactor 出発駅モデルと到着駅モデルには同値性判定が不要になるので削除

--- src/main/kotlin/domain/model/payment/ArrivalStation.kt
+++ src/main/kotlin/domain/model/payment/ArrivalStation.kt
@@ -14,19 +14,4 @@ class ArrivalStation(aStation: Station) {
 
         station = aStation
     }
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as ArrivalStation
-
-        if (station == other.station) return true
-
-        return false
-    }
-
-    override fun hashCode(): Int {
-        return station.name.hashCode()
-    }
 }
