63054d08635c00ea2e34aed398781f0ed6fa0f01
MikiWaraMiki <a.i.u.e.o.1.2.3.a@gmail.com>
2021-09-26 13:54:18 +0900

    add Station EnumクラスにLabelからStationオブジェクトを生成できるメソッドを追加

--- src/main/kotlin/domain/model/station/Station.kt
+++ src/main/kotlin/domain/model/station/Station.kt
@@ -3,8 +3,18 @@ package domain.model.station
 /**
  * 駅
  */
-enum class Station() {
-    TOKYO,
-    SHIN_OSAKA,
-    HIMEJI;
+enum class Station(private val label: String) {
+    TOKYO("tokyo"),
+    SHIN_OSAKA("shin_osaka"),
+    HIMEJI("himeji");
+
+
+    companion object {
+        fun fromLabel(label: String): Station {
+            val station = values().firstOrNull() { it.label == label } ?:
+                throw IllegalArgumentException("存在しない駅です")
+
+            return station
+        }
+    }
 }
