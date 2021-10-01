c3df6305878f6873bfed08235fe3a0948aa65cd9
TokorogaDokkoi <46447946+MikiWaraMiki@users.noreply.github.com>
2021-10-01 21:59:49 +0900

    add 経路表という概念を追加した

---
+++ src/main/kotlin/domain/model/route/JrRouteTable.kt
@@ -0,0 +1,23 @@
+package domain.model.route
+
+import domain.model.station.Station
+
+/**
+ * JR移動可能な経路が登録された表
+ */
+class JrRouteTable {
+    private val routeTable = mapOf<Station, List<Station>>(
+        Station.TOKYO to listOf<Station>(Station.HIMEJI, Station.SHIN_OSAKA)
+    )
+
+    fun of(departureStation: Station, arrivalStation: Station): Route {
+        val arrivalStationList = routeTable[departureStation] ?:
+            throw IllegalArgumentException("経路表に登録されていない出発駅です")
+
+        if (!arrivalStationList.contains(arrivalStation)) {
+            throw IllegalArgumentException("経路表に登録されていない到着駅です")
+        }
+
+        return Route(departureStation, arrivalStation)
+    }
+}
--- src/main/kotlin/domain/model/route/Route.kt
+++ src/main/kotlin/domain/model/route/Route.kt
@@ -22,6 +22,7 @@ class Route(
 
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
+
         if (javaClass != other?.javaClass) return false
 
         other as Route
