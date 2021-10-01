371bc7be6c10926d0e47b62ab64510c23fde6a39
TokorogaDokkoi <46447946+MikiWaraMiki@users.noreply.github.com>
2021-10-01 21:50:47 +0900

    add route クラスをrouteパッケージに移動する
    sharedに存在することに違和感を感じたので、経路パッケージを作成し移動する

---
+++ src/main/kotlin/domain/model/route/Route.kt
@@ -0,0 +1,38 @@
+package domain.model.route
+
+import domain.model.station.Station
+
+/**
+ * 経路
+ */
+class Route(
+    aDepartureStation: Station,
+    anArrivalStation: Station
+) {
+    val departureStation: Station
+    val arrivalStation: Station
+
+    init {
+        if (aDepartureStation == anArrivalStation)
+            throw IllegalArgumentException("出発駅と到着駅は同じにすることはできません")
+
+        departureStation = aDepartureStation
+        arrivalStation = anArrivalStation
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Route
+
+        if (departureStation == other.departureStation && arrivalStation == other.arrivalStation)
+            return true
+
+        return false
+    }
+
+    override fun hashCode(): Int {
+        return (departureStation.hashCode() + arrivalStation.hashCode()) * 31
+    }
+}
