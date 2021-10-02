e58d88d735adef76caabf6e229e8060b5ac3f5f1
TokorogaDokkoi <46447946+MikiWaraMiki@users.noreply.github.com>
2021-10-02 16:54:01 +0900

    fix 搭乗日追加による影響の修正もれ

--- src/test/kotlin/domain/model/discount/DistanceKilometerDiscountRuleTest.kt
+++ src/test/kotlin/domain/model/discount/DistanceKilometerDiscountRuleTest.kt
@@ -1,8 +1,9 @@
 package domain.model.discount
 
-import domain.model.discount.roundtrip.DistanceKilometerDiscountRule
+import domain.model.discount.rule.DistanceKilometerDiscountRule
 import domain.model.route.Route
 import domain.model.station.Station
+import domain.model.ticket.DepartureDate
 import domain.model.ticket.Ticket
 import domain.model.ticket.TicketType
 import domain.model.train.SeatType
@@ -11,6 +12,7 @@ import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import java.time.LocalDate
 
 class DistanceKilometerDiscountRuleTest {
     @Nested
@@ -20,6 +22,7 @@ class DistanceKilometerDiscountRuleTest {
         fun `片道の場合はfalseを返すこと`() {
             val ticket = Ticket(
                 route = Route(Station.TOKYO, Station.HIMEJI),
+                departureDate = DepartureDate(LocalDate.now()),
                 ticketType = TicketType.ONE_WAY,
                 trainType =  TrainType.HIKARI,
                 seatType = SeatType.NON_RESERVED,
@@ -35,6 +38,7 @@ class DistanceKilometerDiscountRuleTest {
         fun `往復の場合、片道の移動距離が601キロ以下の場合は、falseを返すこと`() {
             val ticket = Ticket(
                 route = Route(Station.TOKYO, Station.SHIN_OSAKA),
+                departureDate = DepartureDate(LocalDate.now()),
                 ticketType = TicketType.ROUND_TRIP,
                 trainType = TrainType.HIKARI,
                 seatType = SeatType.NON_RESERVED,
@@ -50,6 +54,7 @@ class DistanceKilometerDiscountRuleTest {
         fun `往復の場合、片道の移動距離が601キロ以上の場合は、trueを返すこと`() {
             val ticket = Ticket(
                 route = Route(Station.TOKYO, Station.HIMEJI),
+                departureDate = DepartureDate(LocalDate.now()),
                 ticketType = TicketType.ROUND_TRIP,
                 trainType = TrainType.HIKARI,
                 seatType = SeatType.NON_RESERVED,
