458db2ea73deafc9846286bba044bbc84f766288
TokorogaDokkoi <46447946+MikiWaraMiki@users.noreply.github.com>
2021-10-31 12:11:57 +0900

    remove discount class

--- src/main/kotlin/domain/model/discount/DiscountResult.kt
+++ src/main/kotlin/domain/model/discount/DiscountResult.kt
@@ -7,9 +7,4 @@ import domain.model.shared.Price
  * 割引額計算結果クラス
  */
 class DiscountResult {
-    private val discountList = mutableListOf<Discount>()
-
-    fun addDiscount(discount: Discount) {
-        discountList.add(discount)
-    }
 }
