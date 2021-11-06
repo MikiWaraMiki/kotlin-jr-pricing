64f929175b9a9289f8a6bb1c7afca01fa77755ef
TokorogaDokkoi <46447946+MikiWaraMiki@users.noreply.github.com>
2021-11-06 16:39:26 +0900

    fix 特別団体割引の特急料金割引

---
+++ src/main/kotlin/domain/model/discount/largegroup/LargeGroupSurchargeDiscount.kt
@@ -0,0 +1,30 @@
+package domain.model.discount.largegroup
+
+import domain.model.discount.DiscountName
+import domain.model.discount.Discount
+import domain.model.fare.Fare
+import domain.model.shared.Passengers
+import domain.model.shared.Price
+import domain.model.surcharge.Surcharge
+
+/**
+ * 特別団体割引クラス
+ */
+class LargeGroupSurchargeDiscount(
+    private val surcharge: Surcharge,
+    private val passengers: Passengers
+    ): Discount {
+    override val discountName = DiscountName("団体割引（乗員人数が31人以上の場合に適用）")
+
+    private val freePassengerCount = FreePassengerCount(passengers)
+
+    fun afterDiscounted(): Price {
+        val adults = passengers.adults - freePassengerCount.adultDiscountNumber()
+        val childs = passengers.childs - freePassengerCount.childDiscountNumber()
+
+        val adultResult = surcharge.price(false).value * adults
+        val childResult = surcharge.price(true).value * childs
+
+        return Price.of(adultResult + childResult)
+    }
+}
