e0dc0fcba322c5738e9593d85630eee89e8afe10
TokorogaDokkoi <46447946+MikiWaraMiki@users.noreply.github.com>
2021-11-07 12:06:33 +0900

    refactor 電車種別で変動する料金を別クラスに分ける

---
+++ src/main/kotlin/domain/model/surcharge/TrainTypeSurcharge.kt
@@ -0,0 +1,32 @@
+package domain.model.surcharge
+
+import domain.model.route.Route
+import domain.model.shared.Price
+import domain.model.surcharge.additionalcharge.NozomiAdditionalChargeTable
+import domain.model.train.TrainType
+
+/**
+ * 電車種別特急料金
+ */
+class TrainTypeSurcharge private constructor(
+    private val baseSurcharge: Surcharge,
+    private val route: Route
+) {
+    private val nozomiAdditionalChargeTable = NozomiAdditionalChargeTable()
+
+    private fun applyedSurcharge(): Surcharge {
+        val additionalPrice = nozomiAdditionalChargeTable.price(route)
+
+        val addedPrice = Price(baseSurcharge.price(false).value + additionalPrice.value)
+
+        return Surcharge(addedPrice)
+    }
+
+    companion object {
+        fun from(surcharge: Surcharge, route: Route, trainType: TrainType): Surcharge {
+            if (!trainType.isNozomi()) return surcharge
+
+            return TrainTypeSurcharge(surcharge, route).applyedSurcharge()
+        }
+    }
+}
