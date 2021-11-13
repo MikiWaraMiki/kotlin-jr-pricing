7f8ffe33ab546cd5df973ce3fa0ec278587c56f0
TokorogaDokkoi <46447946+MikiWaraMiki@users.noreply.github.com>
2021-11-13 21:12:39 +0900

    add recontructor

--- src/main/kotlin/jrpricing/catalog/domain/model/station/Station.kt
+++ src/main/kotlin/jrpricing/catalog/domain/model/station/Station.kt
@@ -4,12 +4,16 @@ package jrpricing.catalog.domain.model.station
  * 駅
  */
 class Station private constructor(
-    private val stationId: StationId,
+    val stationId: StationId,
+    val stationName: StationName
 ) {
 
     companion object {
-        fun reConstructor(stationId: StationId) {
-
+        fun reConstructor(stationId: StationId, stationName: StationName): Station {
+            return Station(
+                stationId,
+                stationName
+            )
         }
     }
 }
