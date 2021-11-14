819f4c94f84f22814f396f32feab0a26e41931ab
TokorogaDokkoi <46447946+MikiWaraMiki@users.noreply.github.com>
2021-11-14 18:34:23 +0900

    add distance column

--- jr-pricing-db/src/main/resources/db/migration/V1.02__create_routes_table.sql
+++ jr-pricing-db/src/main/resources/db/migration/V1.02__create_routes_table.sql
@@ -2,6 +2,7 @@ CREATE TABLE tbl_routes (
     id varchar(26) NOT NULL PRIMARY KEY,
     departure_station_id varchar(26) NOT NULL,
     arrival_station_id varchar(26) NOT NULL,
+    distance bigint NOT NULL,
     created_at timestamp NOT NULL default CURRENT_TIMESTAMP,
     updated_at timestamp NOT NULL default CURRENT_TIMESTAMP,
     FOREIGN KEY (departure_station_id) REFERENCES tbl_stations(id),
