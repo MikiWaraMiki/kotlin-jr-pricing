68efceb49c8d6daffd6ce64a3748ea24e08c9019
TokorogaDokkoi <46447946+MikiWaraMiki@users.noreply.github.com>
2021-11-23 17:54:51 +0900

    add のぞみ料金加算メソッド

--- jr-pricing-catalog/src/main/kotlin/jrpricing/catalog/domain/model/surcharge/BasicSurcharge.kt
+++ jr-pricing-catalog/src/main/kotlin/jrpricing/catalog/domain/model/surcharge/BasicSurcharge.kt
@@ -1,6 +1,7 @@
 package jrpricing.catalog.domain.model.surcharge
 
 import jrpricing.catalog.domain.exception.DomainException
+import jrpricing.catalog.domain.exception.ErrorCode
 import jrpricing.catalog.domain.model.fare.BasicFare
 import jrpricing.catalog.domain.model.route.RouteId
 import jrpricing.catalog.domain.model.shared.Amount
@@ -15,6 +16,18 @@ class BasicSurcharge private constructor(
     init {
         // TODO: 運賃と重複がある
         if (amount.value % 10 > 0) throw DomainException("特急料金は10円単位である必要があります")
+    }
+
+    fun addNozomiAdditionalCharge(nozomiAdditionalCharge: NozomiAdditionalCharge): BasicSurcharge {
+        if (routeId != nozomiAdditionalCharge.routeId)
+            throw DomainException("経路が一致しないためのぞみ利用時の料金を加算することができません", ErrorCode.INVALID_INPUT)
+
+        val result = Amount(amount.value.plus(nozomiAdditionalCharge.amount.value))
+
+        return BasicSurcharge.reConstructor(
+            routeId,
+            result
+        )
     }
 
     companion object {
--- jr-pricing-catalog/src/test/kotlin/jrpricing/catalog/domain/model/surcharge/BasicSurchargeTest.kt
+++ jr-pricing-catalog/src/test/kotlin/jrpricing/catalog/domain/model/surcharge/BasicSurchargeTest.kt
@@ -3,46 +3,84 @@ package jrpricing.catalog.domain.model.surcharge
 import jrpricing.catalog.domain.exception.DomainException
 import jrpricing.catalog.domain.model.route.RouteId
 import jrpricing.catalog.domain.model.shared.Amount
-import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.api.*
 
-class BasicSurchargeTest {
-    @Test
-    fun `特急料金に5円単位が利用されている場合はエラーが発生する`() {
-        val routeId = RouteId.create()
-        val amount = Amount(5)
+internal class BasicSurchargeTest {
 
-        val target: () -> Unit = {
-            BasicSurcharge.reConstructor(routeId, amount)
+    @Nested
+    @DisplayName("不変条件テスト")
+    inner class InitializeTest() {
+        @Test
+        fun `特急料金に5円単位が利用されている場合はエラーが発生する`() {
+            val routeId = RouteId.create()
+            val amount = Amount(5)
+
+            val target: () -> Unit = {
+                BasicSurcharge.reConstructor(routeId, amount)
+            }
+
+            val exception = assertThrows<DomainException>(target)
+
+            Assertions.assertEquals("特急料金は10円単位である必要があります", exception.message)
         }
 
-        val exception = assertThrows<DomainException>(target)
+        @Test
+        fun `特急料金に1円単位が利用されている場合はエラーが発生する`() {
+            val routeId = RouteId.create()
+            val amount = Amount(11)
 
-        Assertions.assertEquals("特急料金は10円単位である必要があります", exception.message)
-    }
+            val target: () -> Unit = {
+                BasicSurcharge.reConstructor(routeId, amount)
+            }
 
-    @Test
-    fun `特急料金に1円単位が利用されている場合はエラーが発生する`() {
-        val routeId = RouteId.create()
-        val amount = Amount(11)
+            val exception = assertThrows<DomainException>(target)
 
-        val target: () -> Unit = {
-            BasicSurcharge.reConstructor(routeId, amount)
+            Assertions.assertEquals("特急料金は10円単位である必要があります", exception.message)
         }
 
-        val exception = assertThrows<DomainException>(target)
+        @Test
+        fun `特急料金が10円単位であればエラーが発生しないこと`() {
+            val routeId = RouteId.create()
+            val amount = Amount(10)
 
-        Assertions.assertEquals("特急料金は10円単位である必要があります", exception.message)
+            val basicSurcharge = BasicSurcharge.reConstructor(routeId, amount)
+
+            Assertions.assertEquals(amount, basicSurcharge.amount)
+        }
     }
 
-    @Test
-    fun `特急料金が10円単位であればエラーが発生しないこと`() {
-        val routeId = RouteId.create()
-        val amount = Amount(10)
+    @Nested
+    @DisplayName("addNozomiAdditionalChargeのテスト")
+    inner class AddNozomiAdditionalCharge() {
+        @Test
+        fun `経路が一致する場合は、加算した結果になること`() {
+            val routeId = RouteId.create()
+            val amount = Amount(10)
 
-        val basicSurcharge = BasicSurcharge.reConstructor(routeId, amount)
+            val basicSurcharge = BasicSurcharge.reConstructor(routeId, amount)
+            val nozomiAdditionalCharge = NozomiAdditionalCharge.reConstructor(routeId, amount)
 
-        Assertions.assertEquals(amount, basicSurcharge.amount)
+            val result = basicSurcharge.addNozomiAdditionalCharge(nozomiAdditionalCharge)
+
+            Assertions.assertEquals(Amount(20), result.amount)
+        }
+
+        @Test
+        fun `経路が一致しない場合は、DomainExceptionが発生すること`() {
+            val routeId = RouteId.create()
+            val otherRouteId = RouteId.create()
+            val amount = Amount(10)
+
+            val basicSurcharge = BasicSurcharge.reConstructor(routeId, amount)
+            val nozomiAdditionalCharge = NozomiAdditionalCharge.reConstructor(otherRouteId, amount)
+
+            val target: () -> Unit = {
+                basicSurcharge.addNozomiAdditionalCharge(nozomiAdditionalCharge)
+            }
+
+            val exception = assertThrows<DomainException>(target)
+
+            Assertions.assertEquals("経路が一致しないためのぞみ利用時の料金を加算することができません", exception.message)
+        }
     }
 }
