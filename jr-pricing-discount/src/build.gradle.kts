0ea24965db11044b02f9006604090ded7725fe8c
TokorogaDokkoi <46447946+MikiWaraMiki@users.noreply.github.com>
2021-11-23 12:24:17 +0900

    add discount application

---
+++ jr-pricing-discount/src/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    application
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-jooq")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("com.github.guepardoapps:kulid:2.0.0.0")
+    implementation(project(":jr-pricing-db"))
+    runtimeOnly("org.postgresql:postgresql")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.mockk:mockk:1.12.0")
+}
+
+
+application {
+    mainClass.set("jrpricing.fare.FareApplication")
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,5 +3,6 @@ include(
     "jr-pricing-db",
     "jr-pricing-old-app",
     "jr-pricing-catalog",
-    "jr-pricing-fare"
+    "jr-pricing-fare",
+    "jr-pricing-discount"
 )
